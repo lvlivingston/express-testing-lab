13a3144522cc053f411290bd337958c898d6b735
lvlivingston <lvlivingston@gmail.com>
2023-10-25 20:50:37 -0500

    completed homework

--- tests/app.test.js
+++ tests/app.test.js
@@ -22,36 +22,34 @@ describe('List out all gifs', () => {
     test('Get specific id of a gif', async () => {
         const response = await request(app)
         .get('/gifs/65398cf9e7d1a648124fb983')
-        console.log(response.body)
+        // console.log(response.body)
         expect(response.body._id).toEqual('65398cf9e7d1a648124fb983');
         expect(response.statusCode).toBe(200);
     });
     test('Add a new gif and return that gif', async () => {
         const response = await request(app)
         .post('/gifs')
-        .create({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif' });
-        expect(response.body).toEqual({ id: '123', name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif' });
+        .send({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif' });
+        expect(response.body.url).toEqual('https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif');
         expect(response.statusCode).toBe(200);
     });
     test('Update a new gif and return that gif', async () => {
         const response = await request(app)
-        .put('/gifs/:id')
-        .update({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif' });
-        expect(response.body).toEqual({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif'});
+        .put('/gifs/65398cf9e7d1a648124fb984')
+        .send({ name: 'Beyonce Lemonade' });
+        expect(response.body.name).toEqual('Beyonce Lemonade');
         expect(response.statusCode).toBe(200);
     });
     test('Delete a gif', async () => {
         const response = await request(app)
-        .delete('/gifs/:id')
-        .delete({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif' });
-        expect(response.body).toEqual({ name: 'Beyonce Lemonade Gif', url: 'https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif'});
+        .delete('/gifs/65398cf9e7d1a648124fb984')
+        expect(response.body._id).toEqual('65398cf9e7d1a648124fb984');
         expect(response.statusCode).toBe(200);
     });
 });
     
-// after all tests are finished
 afterAll(done => {
-    // close the server to avoid any hanging requests
     server.close()
+    // process.exit()
     done()
 })
